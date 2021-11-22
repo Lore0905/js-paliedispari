13e545c12aa8981f8da144ac80d6434df2a24abb
Lore0905 <lore.castelli4@gmail.com>
2021-11-22 18:43:07 +0100

    palindroma done

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -10,18 +10,20 @@ console.log(userWordType);
 
 // creo la funzione 
 function palindroma(word){
-
+    let totalWordReverse = '';
+    let typeWord = '';
     // suddivido la parola in lettere con il ciclo FOR ed  inverto le lettere 
-    for( let i = wordRequest.length; i >= 0; i--){
-    let totalWordReverse = wordRequest[i];
+    
 
-    let typeWord;
+    for( let i = word.length - 1; i >= 0; i--){
+    totalWordReverse += word[i];
+        console.log(totalWordReverse);
+    }
 
-    if(totalWordReverse === wordRequest){
+    if(totalWordReverse === word){
         typeWord = 'è una parola palindroma';
     }else{
         typeWord = 'non è una parola palindroma';
     }
-}
-return typeWord;
+    return typeWord;
 }
