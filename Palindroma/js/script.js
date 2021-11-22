82cdb1805243c9024b28770c1990cae372a05ed0
Lore0905 <lore.castelli4@gmail.com>
2021-11-22 16:09:37 +0100

    fot

---
+++ Palindroma/js/script.js
@@ -0,0 +1,15 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente di inserire una parola
+const wordRequest = prompt('Dimmi una Parola');
+
+
+// la suddivido in lettere con il ciclo FOR
+for( let i = wordRequest.length; i = 0; i--){
+
+    console.log(wordRequest[i]);
+    // inverto le lettere 
+}
+   
+// creo la funzione 
